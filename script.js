d597b57392e529b14fabc61325da0e795c41124e
Leonardo Abdalla <leonardoabdalladeveloper@gmail.com>
2021-11-24 22:30:55 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let butao = document.querySelector('#criar-tarefa');
-function salvaTexto() {
-    let digitado = document.querySelector('#texto-tarefa');
-    digitado.innerHTML='leo';
-    console.log(digitado);
+
+function salvaTexto() {  
+  let ul = document.querySelector('#lista-tarefas').innerText;
+  let digitado = document.querySelector('#texto-tarefa').value;
+  ul = ul + "<li>" + digitado + "</li>";
+  document.getElementById("lista-tarefas").innerHTML = ul;
 }
+
 butao.addEventListener('click', salvaTexto);
